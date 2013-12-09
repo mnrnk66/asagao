013520cc18edfd5542cf3d7745cebeecb16a6e89
nkb.sigmix <nakabayashi@sigmix.com>
2013-12-09 17:01:20 +0900

    config/application.rb修正

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module Asagao
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
 
